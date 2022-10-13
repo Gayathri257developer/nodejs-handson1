a5046e8876eb0158b9f45c1d20aca49d40316524
Gayathri D R <99469991+gayathri257developer@users.noreply.github.com>
2022-10-13 16:43:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-   const url = req.url;
-   if(url === '/'){
       res.write('<h1>Nodejs Handson1</h1>');
-      return res.end();
+      res.end();
    }
 });
 
